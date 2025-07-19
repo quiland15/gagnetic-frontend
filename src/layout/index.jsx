cb031d536ceebe1b1de7dbb7d791e79d5523a31e
quiland15 <quiland86@gmail.com>
2025-07-19 12:36:06 +0800

    change layout

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -4,7 +4,7 @@ import Footer from '../components/Footer.jsx'
 
 export default function Layout ({children}){
 	return(
-          <div className="flex flex-col min-h-screen">
+          <div>
             <Header />
             <main className="flex-1">
               {children}
