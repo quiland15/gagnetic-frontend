291caa287eb36f11b7253e4627286c7dd1d5860d
quiland15 <quiland86@gmail.com>
2025-07-19 16:07:53 +0800

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,3 @@
-// import React from 'react'
-// import '../assets/css/home/header.css'
-// import { FiSearch, FiShoppingCart } from 'react-icons/fi';
-
-// export default function Header () {
-// 	return(
-//     <header className="header">
-//       <div className="logo">
-//         <div className="logo-circle">GT</div>
-//         <span className="logo-text">GadgetTech</span>
-//       </div>
-
-//       <nav className="nav-links">
-//         <a href="/" className="active">Home</a>
-//         <a href="/shop">Shop</a>
-//         <a href="/about">About</a>
-//         <a href="/contact">Contact</a>
-//       </nav>
-
-//       <div className="search-cart-login">
-//         <div className="search-box">
-//           <FiSearch className="search-icon" />
-//           <input type="text" placeholder="Search products..." />
-//         </div>
-
-//         <button className="login-btn">Login</button>
-//       </div>
-//     </header>
-// )}
-
-// Header.jsx
 import { Container, Nav, Navbar, Form, FormControl, Button, Badge } from 'react-bootstrap';
 import { FiSearch } from 'react-icons/fi';
 import { FaShoppingCart } from 'react-icons/fa';
