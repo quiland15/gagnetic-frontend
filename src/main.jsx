ed5b07fde4dbd2625705c3c9047aa418e0776063
quiland15 <quiland86@gmail.com>
2025-07-19 16:02:39 +0800

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './assets/css/global/index.css'
+// import './assets/css/global/index.css'
 import Home from './pages/home.jsx'
 import Layout from './layout/index.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
