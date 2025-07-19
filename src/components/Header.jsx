a46ec46da047f15a5cd123c471f4c100dc00648d
quiland15 <quiland86@gmail.com>
2025-07-19 17:22:38 +0800

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,8 @@ export default function Header() {
       <Container fluid>
         {/* Logo */}
         <Navbar.Brand href="/" className="d-flex align-items-center gap-2">
-          <img src={Logo} alt="Logo GT" className="rounded-circle bg-primary text-white fw-bold d-flex align-items-center justify-content-center" style={{ width: '35px', height: '35px' }} />
-          <span className="fw-bold text-primary fs-5">GadgetTech</span>
+          <img src={Logo} alt="Logo GT" className="rounded-circle bg-primary text-white fw-bold d-flex align-items-center justify-content-center" style={{ width: '45px', height: '45px' }} />
+          <span className="fw-bold text-primary fs-5">Gagnetic</span>
         </Navbar.Brand>
 
         {/* Navigation */}
