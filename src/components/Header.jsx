817cd8bcc8d505ba9e6e06de09e060e56704bd8d
quiland15 <quiland86@gmail.com>
2025-07-19 11:56:09 +0800

    just test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header () {
 	return(
           <header className="bg-white shadow-md p-4 flex justify-between items-center">
-            <h1 className="text-2xl font-bold text-blue-600">Gagnetic</h1>
+            <h1 className="text-2xl font-bold text-blue-600">quiland</h1>
             <nav className="space-x-4">
              <a href="/" className="text-gray-700 hover:text-blue-600">Home</a>
              <a href="/about" className="text-gray-700 hover:text-blue-600">About</a>
